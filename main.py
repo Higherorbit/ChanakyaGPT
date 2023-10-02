227cba9ee2b03917ca55b578639909ccfab908f8
Higherorbit <87420218+Higherorbit@users.noreply.github.com>
2023-10-02 23:46:59 +0530

    Added books and preprocessing done

--- main.py
+++ main.py
@@ -6,10 +6,106 @@ import wikipedia
 from config import api_key
 import pyjokes
 import webbrowser
+import string
+import requests
+import re
+from bs4 import BeautifulSoup
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import os
 import openai
 from config import api_key
+from PyPDF2 import PdfReader
+
+def fetch_text_from_pdf(pdf_link):
+    reader=PdfReader(pdf_link)
+    text=""
+    for page in reader.pages:
+        text+=page.extract_text()
+    return text
+
+def preprocess_text(text):
+    text = text.encode('ascii', 'ignore').decode('utf-8')
+    text = text.lower()
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    punctuation = set(string.punctuation)
+    tokens = [token for token in tokens if token not in stop_words and token not in punctuation]
+    return " ".join(tokens)
+
+def collect_data():
+    # Collect and preprocess data from the PDFs
+    # make me a list of sentences
+
+    preprocessedData = []
+    # List of books on Chanakya Neeti with their PDF links
+    books = [
+        {"title": "Chanakya Neeti", "pdf_link": "Chanakya Neeti.pdf"},
+        {"title": "Chanakya in Daily Life", "pdf_link": "Chanakya in Daily Life.pdf"}
+    ]
+    for book in books:
+        pdf_link = book["pdf_link"]
+        text = fetch_text_from_pdf(pdf_link)
+        processed_text = preprocess_text(text)
+        preprocessedData.extend(processed_text.split(".")[:-1])
+
+    return preprocessedData
+
+def create_model(sentences):
+    vocab_size = 10000
+    embedding_dim = 128
+    max_seq_length = 50
+    lstm_units = 256
+    output_units = vocab_size
+
+    tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+    input_sequences = []
+    output_sequences = []
+
+    for sentence_tokens in tokenized_sentences:
+        for i in range(len(sentence_tokens) - 1):
+            input_sequence = sentence_tokens[:i+1]  # Input sequence up to the (i+1)th word
+            output_sequence = sentence_tokens[i+1]   # Output sequence from the (i+1)th word
+            input_sequences.append(input_sequence)
+            output_sequences.append(output_sequence)
+
+    X_train= input_sequences
+    y_train = output_sequences
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
+
+    # Pad sequences
+    # X_train = pad_sequences(X_train, maxlen=max_seq_length)
+    # X_val = pad_sequences(X_val, maxlen=max_seq_length)
+    # y_val = pad_sequences(y_val, maxlen=max_seq_length)
+    # y_train = pad_sequences(y_train, maxlen=max_seq_length)
+
+    # print(X_train)
+    # Build the model
+    model = Sequential([
+        Embedding(vocab_size, embedding_dim, input_length=max_seq_length),
+        LSTM(lstm_units),
+        Dense(output_units, activation='softmax')
+    ])
+
+    # Compile and train the model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10, batch_size=32)
+
+    # Save the trained model
+    model.save('custom_llm_model.h5')
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -127,9 +223,10 @@ def run_wife():
         chat(command)
 
 if __name__=='__main__':
-    while True:
-        cnt=10
-        while cnt:
-            cnt=cnt-1
-            run_wife()
-        break
+    # while True:
+    #     cnt=10
+    #     while cnt:
+    #         cnt=cnt-1
+    #         run_wife()
+    #     break
+    create_model(collect_data())
